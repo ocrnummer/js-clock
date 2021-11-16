f4dd8c7ae52691b2fdd7d4e55fd62fe86ece5535
Alexander <alexanderbergquist80@gmail.com>
2021-11-16 14:00:20 +0100

    Klocka och påskägg

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-// HOHO
+
+
+setTimeout( () => {
+    console.log("Hehe så att säga...");
+}, 5000);
+
+
+
+
+
+
+let clockEl = document.querySelector("#clock")
+let easterEgg = document.querySelector("#easter-egg")
+
+let clock = () => {
+    let date = new Date();
+    let hrs = date.getHours();
+    let mins = date.getMinutes();
+    let secs = date.getSeconds();
+    let time = `${hrs}:${mins}:${secs}`;
+    clockEl.innerText = time;
+}
+
+// let tick = 0;
+setInterval ( () => {
+    // tick++;
+    clock();
+}, 1000);
+
+setTimeout( () => {
+    easterEgg.innerText = "Hehe, om en tänker...";
+}, 10000);
